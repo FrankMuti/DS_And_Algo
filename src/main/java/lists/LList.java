5ee2cf6785978d4a32cf616bdbf9d589d60a0f25
FrankMuti <frankmuti373@gmail.com>
2021-10-11 12:23:55 +0300

    LinkLists

--- src/main/java/lists/LList.java
+++ src/main/java/lists/LList.java
@@ -27,56 +27,90 @@ public class LList<E> implements List<E> {
 
   @Override
   public void insert(E item) {
-
+    curr.setNext(new Link<>(item, curr.next()));
+    if (tail == curr) {
+      tail = curr.next();
+    }
+    cnt++;
   }
 
   @Override
   public void append(E item) {
-
+    tail = tail.setNext(new Link<>(item, null));
   }
 
   @Override
   public E remove() {
-    return null;
+    if (curr.next() == null) {
+      return null;
+    }
+    E item = curr.next().element();
+    if (tail == curr.next()) {
+      tail = curr;
+    }
+    curr.setNext(curr.next().next());
+    cnt--;
+    return item;
   }
 
   @Override
   public void moveToStart() {
-
+    curr = head;
   }
 
   @Override
   public void moveToEnd() {
-
+    curr = tail;
   }
 
   @Override
   public void prev() {
-
+    if (curr == head) {
+      return;
+    }
+    Link<E> tmp = head;
+    while (tmp.next() != curr) {
+      tmp = tmp.next();
+    }
+    curr = tmp;
   }
 
   @Override
   public void next() {
-
+    if (curr != tail) {
+      curr = curr.next();
+    }
   }
 
   @Override
   public int length() {
-    return 0;
+    return cnt;
   }
 
   @Override
   public int currPos() {
-    return 0;
+    Link<E> tmp = head;
+    int i;
+    for (i = 0; curr != tmp; i++) {
+      tmp = tmp.next();
+    }
+    return i;
   }
 
   @Override
   public void moveToPos(int pos) {
-
+    assert (pos >= 0) && (pos <= cnt) : "Position out of range";
+    curr = head;
+    for (int i = 0; i < pos; i++) {
+      curr = curr.next();
+    }
   }
 
   @Override
   public E getValue() {
-    return null;
+    if (curr.next() == null) {
+      return null;
+    }
+    return curr.next().element();
   }
 }
